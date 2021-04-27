b6faf0b60759df42da7303f848eaa5336df8ea92
Nico Sap <sap.werk@gmail.com>
2021-04-27 09:24:00 +0200

    Add functionality to replace files with empty contents

--- CleanupIO/Program.cs
+++ CleanupIO/Program.cs
@@ -9,11 +9,19 @@ namespace CleanupIO
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <example>
+        /// CleanupIO.exe -Folder="D:\MyApp\Devxpress\Source" -Pattern="licenses.licx" -EmptyContents -SubDir -Days=-1 //Empty the contents of all licenses.licx files
+        /// CleanupIO.exe -Folder="C:\MyApp\Logs" -Pattern="*.log" -Days="10" // Cleans up all log files older than 10 days
+        /// </example>
         static void Main(string[] args)
         {
 
 
-            int daysOld = 7;
+            int daysOld = -1;
             string folder = "";
             string pattern = "*.*";
             System.IO.SearchOption searchOption = System.IO.SearchOption.TopDirectoryOnly;
@@ -21,7 +29,7 @@ namespace CleanupIO
 
             if (CommandLine["Help"] != null)
             {
-                Console.Out.WriteLine("-Help, -Folder, -Pattern, -Days");
+                Console.Out.WriteLine("-Help, -Folder, -Pattern, -Days", "-EmptyContents");
             }
             else
             {
@@ -50,18 +58,30 @@ namespace CleanupIO
                     daysOld = int.Parse(CommandLine["Days"]);
                 }
 
+                bool emptyContents = CommandLine["EmptyContents"] != null;
+
                 var CurrentTime = System.DateTime.UtcNow;
                 var pre_matched_files = new System.IO.DirectoryInfo(folder).GetFiles(pattern, searchOption);
-                var matched_files = pre_matched_files.Where(el => el.LastWriteTimeUtc.AddDays(daysOld) < CurrentTime).ToList();
+                var matched_files = pre_matched_files.Where(el => daysOld == -1 ||  el.LastWriteTimeUtc.AddDays(daysOld) < CurrentTime).ToList();
 
                 Console.Out.WriteLine(string.Format("Matched {0} files in folder {1}", matched_files.Count(),folder));
                 foreach (var file in matched_files)
                 {
-                    Console.Out.WriteLine(string.Format("Deleting {0} ...", file.Name));
-                    file.Delete();
+
+                    if(emptyContents)
+                    {
+                        Console.Out.WriteLine(string.Format("Emptying contents {0} ...", file.Name));
+                        System.IO.File.WriteAllText(file.FullName, "");
+                    }
+                    else
+                    {
+                        Console.Out.WriteLine(string.Format("Deleting {0} ...", file.Name));
+                        file.Delete();
+                    }
+                   
                 }
 
-                Console.Out.WriteLine(string.Format("Deleted {0} files ", matched_files.Count()));
+                Console.Out.WriteLine(string.Format("Modified {0} files ", matched_files.Count()));
             }
 
         }
--- README.md
+++ README.md
@@ -7,7 +7,8 @@ CLI for removing temp files and folders (eg. after converting html files to pdf 
 Usage
 =====
 
-CleanupIO.exe -Folder="C:\GeneratedPDF" -Pattern="\*.\*" -Days="10"
+CleanupIO.exe -Folder="C:\MyApp\Logs" -Pattern="*.log" -Days="10" // Cleans up all log files older than 10 days
+CleanupIO.exe -Folder="D:\MyApp\Devxpress\Source" -Pattern="licenses.licx" -EmptyContents -SubDir //Empty the contents of all licenses.licx files
 
 If you want to use this in a recurring way, you can easily use this in the Task Scheduler, which is a standard tool in Windows. Execute the Task Scheduler with Taskschd.msc in the Start Menu - Run (Ctrl + R)
 
@@ -18,3 +19,4 @@ Arguments
 * -Pattern: Delete which fileSearchPattern? (Eg. *.* = all, *.pdf = all pdf files)
 * -Days: The minimal age of the file in days
 * -Subdir: Searches through all directories, leaving this only deletes the matched files in the top directory
+* -EmptyContents: Replace the value in those files with an empty string
